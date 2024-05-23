3a604b4d8337de26f659dd31091a7f9f643e5772
Felipe Santos <felipedev.ti@gmail.com>
2024-05-23 16:16:12 -0300

    Learn: Variaveis
    Estudando sobre variaveis no Javacript.

---
+++ 02-javaScriptp-para-iniciantes/0201-variaveis/script-aula.js
@@ -0,0 +1,24 @@
+
+//formas de declarar variaveis
+var nome = "Luis";
+var idade = 30
+var possuiFaculdade = true;
+console.log(nome,idade,possuiFaculdade,nome);
+
+ //Operações envolvendo Variaveis
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+console.log(totalPreco)
+
+//Posso declarar var sem precisar escrever "var" inumeras vezes.
+var nome = 'Luis' ,
+    sobrenome = 'Felipe';
+console.log(nome,sobrenome)
+
+
+var precoAplicativo;
+// retorna undefined
+console.log(precoAplicativo);
+
+
---
+++ 02-javaScriptp-para-iniciantes/0201-variaveis/script.js
@@ -0,0 +1,26 @@
+
+//Exercicios
+
+// Declarar uma variavel com o seu nome
+var nome = "Luis Felipe";
+   console.log(nome);
+
+// Declarar uma variavel com a  sua idade
+var idade = 30;
+console.log(idade);
+
+// Declarar uma variavel com a  sua comida favorita e não atribuir valor
+var comidaFavorita;
+console.log("comidaFvorita");
+
+// Atribuir valor a sua comida favorita;
+comidaFavorita = "pizza";
+console.log(comidaFavorita);
+
+// Declarar 5 variaveis difrentes sem valor;
+var matematica, fisica, quimica, biologia, cienciasDaNatureza;
+
+
+
+
+
