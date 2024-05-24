369afe0121716a79bef5343851fc07e2716f0df0
Felipe Santos <felipedev.ti@gmail.com>
2024-05-24 18:15:37 -0300

    Learn: Tipos de Dados
    Estudando sobre Strings.
    Aula: 0202-tipos-de-dados

---
+++ 02-javaScriptp-para-iniciantes/0202-tipos-de-dados/script-aula.js
@@ -0,0 +1,23 @@
+var nome = "André";
+var idade = 30;
+//console.log(nome)
+console.log(typeof idade); // ver tipo de dado
+
+// Posso somar strings
+var nome = "Luis ";
+var sobrenome = "Felipe";
+var nomeCompleto = nome + sobrenome
+
+console.log(nomeCompleto)
+
+//Template Strigs 
+var altura = 1.96;
+var voleiJogador = "Giba"
+console.log(`O Joagador de volei ${voleiJogador} tem ${altura}cm.`)
+
+//Utilizando numeros
+var a = 10,
+    b = 15,
+    total = 25;
+
+console.log(`A soma de ${a} mais ${b} é igual a ${total}.`)
---
+++ 02-javaScriptp-para-iniciantes/0202-tipos-de-dados/script-ex.js
@@ -0,0 +1,30 @@
+// Declare uma variável contendo uma string
+
+var livro = "Genesis"
+console.log(livro)
+
+// Declare uma variável contendo um número dentro de uma string
+var livro = "Mateus";
+var capitulo = 28;
+console.log(`Abramos o livro de ${livro} no capitulo ${capitulo}`);
+
+
+// Declare uma variável com a sua idade
+var idade = 30;
+console.log(idade)
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+
+var nome ="Luis "
+var sobrenome ="Silva"
+console.log(nome + sobrenome)
+
+// Coloque a seguinte frase em uma variável: It's time
+
+var frase = "It's time";
+console.log(frase)
+
+// Verifique o tipo da variável que contém o seu nome
+ var meuNome = "Luis";
+ console.log(typeof meuNome)
