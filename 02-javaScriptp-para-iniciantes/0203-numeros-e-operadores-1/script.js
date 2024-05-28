2a912692627e651db8c16696d57909918b0097e5
Felipe Santos <felipedev.ti@gmail.com>
2024-05-28 19:25:27 -0300

    Learn: Numeros e Operadores
    Apredendo sobre Numeros e Operadores.

---
+++ 02-javaScriptp-para-iniciantes/0203-numeros-e-operadores-1/script.js
@@ -0,0 +1,18 @@
+var idade = 2e10;  
+console.log(idade);
+
+var total = idade + 45;
+console.log(total);
+
+// Operações Aritmeticas
+var soma = 100 + 50; // 150
+var subtracao = 100 - 50; // 50
+var multiplicacao = 100 * 2; // 200
+var divisao = 100 / 2; // 50
+var expoente = 2 ** 4; // 16
+var modulo = 14 % 5; // 4
+
+// Is a Number
+var eNumero = 'isso é 10' / 15;
+console.log(isNaN(eNumero))
+
